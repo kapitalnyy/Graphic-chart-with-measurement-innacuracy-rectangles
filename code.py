897e0cd6d0ecc5b3cabb6151ea2c9686fb607d11
Maciej <maciula7878@gmail.com>
2026-01-05 22:39:45 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,21 +1,20 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
-x = [1, 2, 3, 4]
-y = [2.1, 2.9, 3.8, 5.2]
+x = [1,2,10]
+y = [1,2,10]
 
-
-dx = 0.5  
-dy = 0.3  
 
 fig, ax = plt.subplots()
 ax.scatter(x, y, s=2, color='black', zorder=3)
 
 for xi, yi in zip(x, y):
+    dx = 0.01*xi + 0.12
+    dy = 0.008*yi + 0.1
     rect = Rectangle(
-        (xi - dx/2, yi - dy/2), 
-        dx,
-        dy,
+        (xi - dx, yi - dy), 
+        2*dx,
+        2*dy,
         linewidth=0.8,
         edgecolor='blue',
         facecolor='none',
